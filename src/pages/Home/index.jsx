46d0da72500b9c8c10047cf1d19a49553c399109
Bruno Vinícius <94939630+brunowzz@users.noreply.github.com>
2024-02-29 19:21:57 -0300

    :sparkles: feat: add state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./style.module.css";
 
 const Home = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!e) {
+      return;
+    }
+
+    try {
+      setAuthenticated(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <main className={s.container}>
-        <h1 className={s.loginTitle}>Login</h1>
+      {!authenticated && (
+        <main className={s.container}>
+          <h1 className={s.loginTitle}>Login</h1>
 
-        <form className={s.boxForm}>
-          <label className={s.labelFormLogin} htmlFor="email">
-            Email:
-          </label>
-          <input
-            className={s.inputFormLogin}
-            required
-            type="email"
-            name="email"
-          />
+          <form className={s.boxForm} onSubmit={handleSubmit}>
+            <label className={s.labelFormLogin} htmlFor="email">
+              Email:
+            </label>
+            <input
+              className={s.inputFormLogin}
+              required
+              type="email"
+              name="email"
+            />
 
-          <label className={s.labelFormLogin} htmlFor="password">
-            Senha:
-          </label>
-          <input
-            className={s.inputFormLogin}
-            required
-            type="password"
-            name="password"
-          />
+            <label className={s.labelFormLogin} htmlFor="password">
+              Senha:
+            </label>
+            <input
+              className={s.inputFormLogin}
+              required
+              type="password"
+              name="password"
+            />
 
-          <button className={s.buttonSubmitFormLogin} type="submit">
-            Entrar
-          </button>
+            <button className={s.buttonSubmitFormLogin} type="submit">
+              Entrar
+            </button>
 
-          <p className={s.createAccount}>
-            Não tem uma conta? <a href="#">Clique aqui.</a>
-          </p>
-        </form>
-      </main>
+            <p className={s.createAccount}>
+              Não tem uma conta? <a href="#">Clique aqui.</a>
+            </p>
+          </form>
+        </main>
+      )}
+
+      {authenticated && (
+        <main>
+          <h1>Usuário logado com sucesso</h1>
+        </main>
+      )}
     </>
   );
 };
